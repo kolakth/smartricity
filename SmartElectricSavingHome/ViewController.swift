b86f9251892acdda11005ae82465c5318b195e91
kolakth <kolakth@me.com>
2017-10-26 17:50:02 +0700

    Initial Commit

---
+++ SmartElectricSavingHome/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartElectricSavingHome
+//
+//  Created by Kolak Keeratipattarakul on 10/26/17.
+//  Copyright © 2017 SESH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
