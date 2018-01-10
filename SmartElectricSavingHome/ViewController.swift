2b2e900a046b99b1ae230beacc866d5ed81f2f48
kolakth <kolakth@me.com>
2018-01-10 15:55:18 +0700

    First push

--- SmartElectricSavingHome/ViewController.swift
+++ SmartElectricSavingHome/ViewController.swift
@@ -7,19 +7,24 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func mqttDelegateTesting(){
+        //int data = FIRDatabase.database().
+        
+    }
 
 }
 
